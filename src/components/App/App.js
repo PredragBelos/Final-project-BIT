7f0211419ee549aa4d5d618d7ca940370bd1f1c9
PredragBelos <predragbelos@gmail.com>
2021-09-08 19:36:11 +0200

    development - Insert Route and Router elements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
+import { Route } from 'react-router';
+import HomePage from '../Home page/homePage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Route path="/home" component={HomePage}/>
     </div>
   );
 }
