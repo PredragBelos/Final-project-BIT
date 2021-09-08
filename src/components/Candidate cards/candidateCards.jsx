caae073c0ac5a150f7a9021f09fdbb6584759e02
PredragBelos <predragbelos@gmail.com>
2021-09-08 17:18:29 +0200

    development - Create basic documentation structure

---
+++ src/components/Candidate cards/candidateCards.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './css/candidateCards.css';
+
+const CandidateCards = () => {
+    //STATE
+
+    //LIFECICLE
+
+    //FUNCTIONS
+
+    //RENDER
+    return (
+        <div className="candidateCards">
+
+        </div>
+    );
+};
+
+export default CandidateCards;
---
+++ src/components/Search box/searchBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './css/searchBox.css';
+
+const SearchBox = () => {
+    //STATE
+
+    //LIFECICLE
+
+    //FUNCTIONS
+
+    //RENDER
+    return (
+        <div className="searchBox">
+            
+        </div>
+    );
+};
+
+export default SearchBox;
