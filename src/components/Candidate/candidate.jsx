e3210cbadec8e05476cda018aa32192031d902c6
PredragBelos <predragbelos@gmail.com>
2021-09-09 10:06:41 +0200

    tsc-2 Create candidate card component and set media screen

--- src/components/Candidate/candidate.jsx
+++ src/components/Candidate/candidate.jsx
@@ -10,7 +10,7 @@ const CandidateCard = ({name, image, email}) => {
 
     //RENDER
     return (
-        <div className="cardBorder col-3">
+        <div className="cardBorder col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3">
             <div className="card">
                 <div className="candidateImage" style={{backgroundImage: `url(${image})`}}>
                     
