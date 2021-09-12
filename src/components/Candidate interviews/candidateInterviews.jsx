688b074ca16c8c152673065b2a98c6abd213fb37
PredragBelos <predragbelos@gmail.com>
2021-09-12 10:22:37 +0200

    tsc-5 Cretated two empty row in interview table if candidate has't interviews

--- src/components/Candidate interviews/candidateInterviews.jsx
+++ src/components/Candidate interviews/candidateInterviews.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './css/candidateInterviews.css';
 
 
-function CandidateInterviews({candidateInterviews}) {
+function CandidateInterviews({ candidateInterviews }) {
+
     return (
         <section className=" container candidateInterviews">
             <table align="center" className="interviewTable">
@@ -17,21 +18,41 @@ function CandidateInterviews({candidateInterviews}) {
                     </tr>
 
                     {
-                        candidateInterviews.map(interview => {
+                        (candidateInterviews.length > 0)
+                            ? candidateInterviews.map(interview => {
+                                return (
+                                    <tr className="interviewTableRow" key={interview.interviewID}>
 
-                            return (
-                                <tr className="interviewTableRow" key={interview.interviewID}>
+                                        <td>{interview.companyName}</td>
+                                        <td>{interview.getInterviewDate()}</td>
+                                        <td>{interview.status}</td>
+                                        <td className="reportOverview">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2PrxTVZdfUDOdYQsDHBY8wYkw2yPqVjH4a41tQ_vbwr27aQC4id1Mv2LJucuazQQOSe0&usqp=CAU" alt="det.." />
+                                        </td>
 
-                                    <td>{interview.companyName}</td>
-                                    <td>{interview.getInterviewDate()}</td>
-                                    <td>{interview.status}</td>
+                                    </tr>
+                                )
+                            })
+                            :
+                            <>
+                                <tr className="interviewTableRow">
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
                                     <td className="reportOverview">
                                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2PrxTVZdfUDOdYQsDHBY8wYkw2yPqVjH4a41tQ_vbwr27aQC4id1Mv2LJucuazQQOSe0&usqp=CAU" alt="det.." />
                                     </td>
-
                                 </tr>
-                            )
-                        })
+
+                                <tr className="interviewTableRow">
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td className="reportOverview">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2PrxTVZdfUDOdYQsDHBY8wYkw2yPqVjH4a41tQ_vbwr27aQC4id1Mv2LJucuazQQOSe0&usqp=CAU" alt="det.." />
+                                    </td>
+                                </tr>
+                            </>
                     }
 
                 </tbody>
