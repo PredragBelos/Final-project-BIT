284178faddd43b652dd466e4092d36c266ba6eeb
PredragBelos <predragbelos@gmail.com>
2021-09-13 11:45:25 +0200

    tsc-I Create Report page header component

--- src/components/Report page header/reportPageHeader.jsx
+++ src/components/Report page header/reportPageHeader.jsx
@@ -12,15 +12,24 @@ const ReportPageHeader = () => {
     //RENDER
     return (
         <header className="container-flow reportPageHeader" >
-            <div className=" container reportHeaderContainer">
-                <div className="reportsLog">
-                    <p className="reportLogoText">Reports Administration</p>
+
+            <div className="container reportHeaderContainer">
+
+                <div className="row reportHeaderRow">
+
+                    <div className="reportsL">
+                        <p className="reportLogoText">Reports Administration</p>
+                    </div>
+
+                    <div className="reportsOpt">
+                        <div className="previewReports btnStile">Reports</div>
+                        <div className="createReportOpt btnStile">Crete Report</div>
+                    </div>
+
                 </div>
-                <div className="reportsOptions">
-                    <div className="previewReports">Reports</div>
-                    <div className="createReportOpt">Crete Report</div>
-                </div>
+
             </div>
+
         </header>
     );
 }
